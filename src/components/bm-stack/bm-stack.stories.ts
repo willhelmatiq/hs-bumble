90d16024b8cd3db507a4e4a029172a5b29a31e56
willhelmatiq <rot3ad@gmail.com>
2022-12-15 21:32:27 +0100

    added stack + fix for button

---
+++ src/components/bm-stack/bm-stack.stories.ts
@@ -0,0 +1,37 @@
+import "../../design-tokens/css/_variables.css";
+
+
+export default {
+  title: 'Components/Stack',
+  // Learn more about how to set up controls at https://storybook.js.org/docs/web-components/essentials/controls#annotation
+  argTypes: {
+    orientation: { control: 'radio', options: ['vertical', 'horizontal'] },
+    gap: { control: 'radio', options: ['large', 'small'] },
+  }
+};
+
+const Template = (args) => `
+  <style>
+    .stack-item { display: inline-block; width: 150px; height: 50px; }
+    .stack-item:nth-child(even) { background-color: var(--hs-color-background-color-background-primary); }
+    .stack-item:nth-child(odd) { background-color: var(--hs-color-typography-header-color-header-neutralcool-darker); }
+  </style>
+  <bm-stack orientation="${args.orientation}" gap="${args.gap}">
+    <span class="stack-item span-1"></span>
+    <span class="stack-item span-2"></span>
+    <span class="stack-item span-3"></span>
+    <span class="stack-item span-4"></span>
+    <span class="stack-item span-5"></span>
+    <span class="stack-item span-6"></span>
+    <span class="stack-item span-7"></span>
+    <span class="stack-item span-8"></span>
+    <span class="stack-item span-9"></span>
+    <span class="stack-item span-10"></span>
+  </bm-stack>
+`;
+
+export const StackStory = Template.bind({});
+StackStory.args = {
+  orientation: 'vertical',
+  gap: 'small',
+};
---
+++ src/components/bm-stack/readme.md
@@ -0,0 +1,18 @@
+# bm-stack
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property      | Attribute     | Description | Type                                                                       | Default      |
+| ------------- | ------------- | ----------- | -------------------------------------------------------------------------- | ------------ |
+| `gap`         | `gap`         |             | `"large" \| "none" \| "small"`                                             | `'small'`    |
+| `orientation` | `orientation` |             | `"horizontal" \| "horizontal-reverse" \| "vertical" \| "vertical-reverse"` | `'vertical'` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
